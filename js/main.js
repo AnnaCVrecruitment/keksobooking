0145f07f6d8311183f65a7a45f6484a8017f6a32
Ann <64134468+AnnaCVrecruitment@users.noreply.github.com>
2021-02-04 12:51:53 +0300

    исправила функции

--- js/main.js
+++ js/main.js
@@ -1,24 +1,33 @@
+
 function getRandomIntFromRange(min, max) {
+
   min = Math.ceil(min);
   max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min;
-}
 
   if (min<0) {
     console.log('Поменяйте первый параметр');
   }
+
   if (max<0) {
     console.log('Поменяйте второй параметр');
   }
+
   if (max<min) {
-    console.log('Поменяйте параметры, чтобы max был больше min');
+    console.log('Так как max должно быть больше min, поменяйте их местами');
   }
+
+  return Math.floor(Math.random() * (max - min)) + min;
+
+}
 
 
 console.log(getRandomIntFromRange(5, 10));
 
 
-function getRandomFloat(min, max) {
-  return Math.random() * (max - min) + min;
+
+
+function getRandomFloat(min, max, n) {
+  return (Math.random() * (max - min) + min).toFixed(n);
 }
-getRandomFloat(11, 101)
+
+console.log(getRandomFloat(11, 101, 3));
